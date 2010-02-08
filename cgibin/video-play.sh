e8b99272dc77b712572f1e5174e99ec93629c6dd
Mark Gibson <mark@mark-eeepc.(none)>
2010-02-08 19:13:06 +0000

    fix

--- cgibin/video-play.sh
+++ cgibin/video-play.sh
@@ -3,7 +3,7 @@
 export DISPLAY
 echo "Content-type: text/plain"
 echo
-f=$(echo $1 | sed -r "s/%20/ /")
+f=$(echo $1 | sed -r "s/%20/ /g")
 echo "$PWD/files/$f"
 /usr/bin/vlc --fullscreen --play-and-exit "$PWD/files/$f" >/dev/null 2>&1 &
 
